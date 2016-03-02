187572a8c77c1f94be942a3de40d1c8b765cfe25
Rusi Filipov <rfilipov@gmail.com>
2016-03-02 22:04:57 +0100

    Fix error handling

--- src/main/java/scoreboard/Starter.java
+++ src/main/java/scoreboard/Starter.java
@@ -26,7 +26,7 @@ public class Starter {
 		historyFile.createNewFile();
 		history.replay(historyFile);
 		InputAdapter input = new InputAdapter(reader, history);
-		input.run();
+		input.runAllCommands();
 		history.write(historyFile);
 	}
 
